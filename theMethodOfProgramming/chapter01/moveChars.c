74e53847925790083265d5bc8c08ab28537718fc
tangchao <tangchao@certusnet.com.cn>
2018-03-08 19:57:24 +0800

    2018.03.08..

--- theMethodOfProgramming/chapter01/moveChars.c
+++ theMethodOfProgramming/chapter01/moveChars.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+// solution_tc 
 void moveChars(char *str, unsigned int n)
 {
     unsigned int len = strlen(str);
@@ -17,6 +19,24 @@ void moveChars(char *str, unsigned int n)
     }
 }
 
+// solution_01
+void LeftShiftOne(char* s, int n)
+{
+    char t = s[0];      //保存第一个字符     
+    for (int i = 1; i < n; i++)     
+    {
+        s[i - 1] = s[i];
+    }
+    s[n - 1] = t;
+}
+
+void LeftRotateString(char* s, int n, int m)
+{
+    while (m--)
+    {
+        LeftShiftOne(s, n);
+    }
+}
 
 int main(int argc, char const *argv[])
 {
