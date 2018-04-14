b1e460db45da0c0c906ad04297342f9113e079c0
tangchao <tangchao@certusnet.com.cn>
2018-04-14 09:44:19 +0800

    2018.04.13

---
+++ theMethodOfProgramming/chapter02/sumOfkNumber.c
@@ -0,0 +1,10 @@
+void SumOfkNumber(int sum, int n)
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+    SumOfkNumber(10, 10);
+    return 0;
+}
--- theMethodOfProgramming/chapter02/twoSum.c
+++ theMethodOfProgramming/chapter02/twoSum.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/* solution_tc
 void TwoSum(int data[], unsigned int length, int sum)
 {
     int i, j;
@@ -16,11 +17,42 @@ void TwoSum(int data[], unsigned int length, int sum)
     }
     printf("Can not find!!!\n");
 }
+*/
+
+/* solution_01 */
+void TwoSum(int data[], unsigned int length, int sum)
+{
+    //sort(s, s+n);   如果数组非有序的，那就事先排好序O(N log N)
+    int begin = 0;
+    int end = length - 1;
+
+    //俩头夹逼，或称两个指针两端扫描法，很经典的方法，O(N)
+    while (begin < end)
+    {
+        long currSum = data[begin] + data[end];
+        if (currSum == sum)
+        {
+            //题目只要求输出满足条件的任意一对即可
+            printf("%d %d\n", data[begin], data[end]);
+            //如果需要所有满足条件的数组对，则需要加上下面两条语句：
+            //begin++
+            //end--
+            break;
+        }
+        else
+        {
+            if (currSum < sum)
+                begin++;
+            else
+                end--;
+        }
+    }
+}
 
 int main(int argc, char const *argv[])
 {
     /* code */
-    int test[] = {1, 2, 4, 6, 11, 17};
-    TwoSum(test, sizeof(test)/sizeof(int), 27);
+    int test[] = {1, 2, 4, 7, 11, 15};
+    TwoSum(test, sizeof(test) / sizeof(int), 15);
     return 0;
 }
