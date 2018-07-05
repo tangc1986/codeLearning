40ec0d2fa452ffa0e97b2a4979caf83518ed0055
tangc1986 <tangc1986@gmail.com>
2018-07-06 00:06:59 +0800

    2018.07.06

--- UULP/chapter02/my_ac2.c
+++ UULP/chapter02/my_ac2.c
@@ -3,9 +3,12 @@
 #include    <utmp.h>
 #include    <fcntl.h>
 #include    <time.h>
+#include    <string.h>
+#include    <errno.h>
+
 
 #define SHOW_ORIGIN
-#define ANALYZE_DATA
+//#define ANALYZE_DATA
 
 typedef struct _link_node
 {
@@ -19,16 +22,50 @@ typedef struct _link_node
 int travel_wtmp(st_link_node **stppLink);
 char *type_to_str(short type);
 
-//int find_matched_process(st_link_node *stpLink, struct utmp *stpData);
+int find_matched_process(st_link_node *stpLink, struct utmp *stpData);
+void show_stored_data(st_link_node *stppLink);
 
 
 int main()
 {
     st_link_node *link_head = NULL;
+    int i = 0;
+
+#ifdef SHOW_ORIGIN
+    printf("%15s%10s%10s%15s%30s%30s\n", "PROC_TYPE", "PID", "DEVICE", "USER", "HOST", "TIME");
+    for (i = 0; i < 110; i++) printf("=");
+    printf("\n");
+    travel_wtmp(&link_head);
+#endif
+    
+#ifdef ANALYZE_DATA
+    printf("%15s%10s%10s%15s%30s%30s%30s\n", "PROC_TYPE", "PID", "DEVICE", "USER", "HOST", "BEGIN_TIME", "END_TIME");
+    for (i = 0; i < 140; i++) printf("=");
+    printf("\n");
 
     travel_wtmp(&link_head);
+
+    show_stored_data(link_head);
+#endif
     
     return 0;
+}
+
+void show_stored_data(st_link_node *stpLink)
+{
+    while (stpLink != NULL)
+    {
+        printf("%15s", type_to_str(stpLink->data.ut_type));
+        printf("%10d", stpLink->data.ut_pid);
+        printf("%10s", stpLink->data.ut_line);
+        printf("%15s", stpLink->data.ut_user);
+        printf("%30s", stpLink->data.ut_host);
+        showtime(stpLink->data.ut_time);
+        showtime(stpLink->endata.ut_time);
+        printf("\n");
+    
+        stpLink = stpLink->next;
+    }
 }
 
 void showtime(time_t timeval)
@@ -43,7 +80,6 @@ void showtime(time_t timeval)
 
 int travel_wtmp(st_link_node **stppLink)
 {
-    int i = 0;
     int fd = 0;
     int iRdNum = 0;
     int iEntSize = sizeof(struct utmp);
@@ -52,40 +88,36 @@ int travel_wtmp(st_link_node **stppLink)
     struct utmp stFix;
     st_link_node *stpCur = *stppLink;
 
-#ifdef SHOW_ORIGIN
-    printf("%15s%10s%10s%15s%30s%30s\n", "PROC_TYPE", "PID", "DEVICE", "USER", "HOST", "TIME");
-    for (i = 0; i < 110; i++) printf("=");
-    printf("\n");
-#endif
 
     if ((fd = open(WTMP_FILE, O_RDONLY)) < 0)
     {
-        //fprintf(stderr, "open error: %s\n", strerror(errno));
+        fprintf(stderr, "open error: %s\n", strerror(errno));
         return -1;
     }
     
     while ((iRdNum = read(fd, &stEntry, iEntSize)) == iEntSize)
     {
-        st_link_node *stpNode = NULL;
-
 #ifdef SHOW_ORIGIN
         printf("%15s", type_to_str(stEntry.ut_type));
         printf("%10d", stEntry.ut_pid);
         printf("%10s", stEntry.ut_line);
-        printf("%15s", stEntry.ut_name);
+        printf("%15s", stEntry.ut_user);
         printf("%30s", stEntry.ut_host);
         showtime(stEntry.ut_time);
         printf("\n");
 #endif
+
+#ifdef ANALYZE_DATA
+    
+        st_link_node *stpNode = NULL;
         
-#if 0
         if (stEntry.ut_type == DEAD_PROCESS || stEntry.ut_type == RUN_LVL)
         {
             find_matched_process(*stppLink, &stEntry);
             continue;
         }
 
-        if (strcmp(stEntry.ut_user, BOOT_USER) == 0)
+        if (strcmp(stEntry.ut_user, BOOT_USER) == 0)        // 为什么这里处理完不continue？？
             find_matched_process(*stppLink, &stEntry);
 
         if ((stpNode = malloc(sizeof(st_link_node))) == NULL)
@@ -107,7 +139,7 @@ int travel_wtmp(st_link_node **stppLink)
 #endif
     }
 
-#if 0
+#ifdef ANALYZE_DATA
     memset(&stFix, 0x00, sizeof(struct utmp));
     stFix.ut_type = RUN_LVL;
     strcpy(stFix.ut_user, SHUT_USER);
@@ -142,8 +174,7 @@ char *type_to_str(short type)
     }
 }
 
-
-#if 0
+#ifdef ANALYZE_DATA
 
 int find_matched_process(st_link_node *stpLink, struct utmp *stpData)
 {
@@ -157,7 +188,7 @@ int find_matched_process(st_link_node *stpLink, struct utmp *stpData)
                        strcmp(stpData->ut_line, stpLink->data.ut_line) == 0 &&
                        strcmp (stpData->ut_host, stpLink->data.ut_host) == 0) || 
                      ( stpData->ut_type == RUN_LVL &&
-                       strcmp(stpData->ut_user, SHUT_USER) == 0))
+                       strcmp(stpData->ut_user, SHUT_USER) == 0))       // SHUT_USER="shutdown"
                 {
                     memcpy(&(stpLink->endata), stpData, sizeof(struct utmp));
                     stpLink->iFlag = MATCHED;
